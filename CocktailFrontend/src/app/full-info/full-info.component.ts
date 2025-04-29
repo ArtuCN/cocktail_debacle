a10ec77bf0baf47bb7e1e4b856758a0f4b8a742d
ArtuCN <arturo.conti.2004@gmail.com>
2025-04-29 20:07:08 +0200

    aggiunto fullInfo

--- CocktailFrontend/src/app/full-info/full-info.component.ts
+++ CocktailFrontend/src/app/full-info/full-info.component.ts
@@ -15,7 +15,8 @@ export class FullInfoComponent implements OnInit {
   cocktail: CocktailInterface = {} as CocktailInterface;
   selectedLanguage: string = 'EN';
   selectedLang: keyof CocktailInterface = 'strInstructions';
-
+  showLanguages: boolean = false;
+  
   availableLanguages: { label: string; key: keyof CocktailInterface, flag: string }[] = [
     { label: 'English', key: 'strInstructions', flag: 'uk.png' },
     { label: 'Italiano', key: 'strInstructionsIT', flag: 'it.png' },
@@ -25,6 +26,8 @@ export class FullInfoComponent implements OnInit {
     { label: '中文 (Semplificato)', key: 'strInstructionsZH_HANS', flag: 'ch.png' },
     { label: '中文 (Tradizionale)', key: 'strInstructionsZH_HANT', flag: 'tw.png' }
   ];
+  ingredients: string[] = [];
+  selectedCocktail: any = null;
 
   currentFlag: string = '';  // Variabile per contenere la bandiera da visualizzare
 
@@ -38,6 +41,7 @@ export class FullInfoComponent implements OnInit {
           if (data.drinks && data.drinks.length > 0) {
             this.cocktail = data.drinks[0];
             console.log(this.cocktail.strDrink);
+            this.loadCocktailDetails(this.cocktail); // ✅ chiamiamolo subito
           }
         },
         error: (err: any) => {
@@ -45,7 +49,38 @@ export class FullInfoComponent implements OnInit {
         }
       });
   }
-
+  
+    
+    toggleLanguageMenu(): void {
+      this.showLanguages = !this.showLanguages;
+    }
+    
+    loadCocktailDetails(cocktail: any) {
+      console.log("loadCocktailDetails");
+      this.selectedCocktail = cocktail;
+      this.ingredients = [];
+    
+      for (let i = 1; i <= 15; i++) {
+        const ingredient = cocktail[`strIngredient${i}`];
+        const measure = cocktail[`strMeasure${i}`];
+    
+        if (ingredient) {
+          const fullIngredient = `${ingredient}`.trim();
+          this.ingredients.push(fullIngredient);
+          console.log(`Ingrediente ${i}:`, fullIngredient);
+        }
+      }
+    
+      console.log("Ingredienti finali:", this.ingredients);
+    }
+    
+    getIngredientImageUrl(ingredient: string): string {
+      const baseUrl = 'https://www.thecocktaildb.com/images/ingredients/';
+      let ingredientName = ingredient.toLowerCase().replace(/\s+/g, '%20');
+      return `${baseUrl}${ingredientName}-small.png`;
+    }
+    
+    
   setLanguage(lang: keyof CocktailInterface): void {
     this.selectedLang = lang;
     const selectedLanguage = this.availableLanguages.find(l => l.key === lang);
